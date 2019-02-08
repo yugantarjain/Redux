bc39d0b592eb26476c19f0ad8c124e1fabbc1ec3
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-08 13:36:17 +0530

    info plist added, ui button tested.

--- Redux/FirstViewController.swift
+++ Redux/FirstViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    let screenSize = UIScreen.main.bounds;
+    
+    
+    let ideaCard = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ideaCard.setTitle("abc", for: .normal)
+        ideaCard.backgroundColor = UIColor.blue
+        view.addSubview(ideaCard)
+        
     }
 
 
