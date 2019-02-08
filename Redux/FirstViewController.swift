17f54ed48dc9d0af24e993b57fc6272ed2c0e393
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-08 12:57:44 +0530

    Initial Commit

---
+++ Redux/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Redux
+//
+//  Created by yugantar jain on 08/02/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Redux/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Redux
+//
+//  Created by yugantar jain on 08/02/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
