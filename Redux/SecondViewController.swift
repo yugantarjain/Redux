366ef6388707c6e2c21124ead1be6018b0409f56
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-09 00:28:28 +0530

    give/take main updated.

--- Redux/SecondViewController.swift
+++ Redux/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "1.jpg")!)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
