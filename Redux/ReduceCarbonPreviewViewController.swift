1b44767eeff6fe86a050c452cf5750472de40d47
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-08 23:39:22 +0530

    reduce carbon init.

---
+++ Redux/ReduceCarbonPreviewViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ReduceCarbonPreviewViewController.swift
+//  Redux
+//
+//  Created by yugantar jain on 08/02/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+
+class ReduceCarbonPreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "1.jpg"))
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
