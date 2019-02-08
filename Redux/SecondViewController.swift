d1c8f3872f9e3e0799468b81c917a147d4b1a981
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-09 02:22:23 +0530

    tbc

--- Redux/SecondViewController.swift
+++ Redux/SecondViewController.swift
@@ -17,29 +17,7 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(patternImage: UIImage(named: "1.jpg")!)
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let tap = UIGestureRecognizer(target: self, action: "Give")
-        for btn in giveButtons
-        {
-            btn.addGestureRecognizer(tap)
-            nextTitle = btn.titleLabel?.text
-        }
     }
-
-    @IBOutlet var giveButtons: [UIButton]!
-    
-    func Give()
-    {
-        performSegue(withIdentifier: "toMain", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let next = segue.destination as! GiveTakeMainViewController
-        next.navTitle = nextTitle
-        
-    }
-    
-    
     
 }
 
