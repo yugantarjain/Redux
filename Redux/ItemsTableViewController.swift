a7aaff3e8d3228d66cb8e4a76deb27183ebfce7e
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-09 08:40:34 +0530

    tbc

---
+++ Redux/ItemsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemsTableViewController.swift
+//  Redux
+//
+//  Created by yugantar jain on 09/02/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
